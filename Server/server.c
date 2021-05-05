65290e1d6f1905aa2000ae512d7a2ceca640fcb8
veve1904 <olivier.dewalque@student.vinci.be>
2021-05-05 11:47:14 +0200

    Add changes in server.c

--- Server/server.c
+++ Server/server.c
@@ -6,7 +6,6 @@
 #define BACKLOG 5
 #define SERVER_PORT 9502
 
-
 // PRE:  ServerPort: a valid port number
 // POST: on success bind a socket to 0.0.0.0:port and listen to it
 //       return socket file descriptor
@@ -29,10 +28,32 @@ int initSocketServer(int port) {
   return sockfd;
 }
 
+
+void socketHandler(void* arg1) {
+    int newsockfd = *(int *)arg1;
+    printf("Numéro du socket dans fils : %d\n",newsockfd);
+    char msg[10];
+    sread(newsockfd,msg,sizeof(msg));
+    printf("message du client : %s\n",msg);
+}
+
 int main (int argc, char ** argv){
-   /* int sockfd, newsockfd*/;
+    int sockfd, newsockfd;
 
     //Pour plus tard : remplacer la constante SERVER_PORT par le premier argument
-    //sockfd = initSocketServer(SERVER_PORT);	
-	//printf("Le serveur tourne sur le port : %i \n",SERVER_PORT);
+    sockfd = initSocketServer(SERVER_PORT);	
+    printf("Le serveur tourne sur le port : %i \n",SERVER_PORT);
+
+    while (1){
+        printf("Le serveur attend une connexion\n");
+
+        newsockfd = saccept(sockfd);
+        //Si le socket a bien été accepté
+        if (newsockfd > 0 ){
+            printf("Numéro du socket dans parent : %d\n",newsockfd);
+            void *ptr = &newsockfd;
+            pid_t childID = fork_and_run1(socketHandler,ptr);
+            printf("Id du child : %d\n",childID);
+        }
+    }
 }
